e40e93b1eb914cfab363bdf31454cba8f72649e9
Myl-W <mylene.wan@gmail.com>
2023-08-31 04:53:40 +0200

    finitions

--- kasa-master/README.md
+++ kasa-master/README.md
@@ -1,3 +1,5 @@
+## 📎 Projet 8 : Kasa - Agence de location immobilière
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -68,3 +70,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+![GitHub repo size]
--- kasa-master/src/App.js
+++ kasa-master/src/App.js
@@ -1,40 +1,34 @@
-import './App.scss';
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import About from './about/about.js'
-import Details from './details/details.js'
-import Home from './home/home.js'
-import NotFound from './404.js'
-import logo from './assets/Logo.png'
-import Header from './components/Header'
-import Footer from './components/Footer';
+import Home from './pages/home/Home';
+import NotFound from './pages/notFound/NotFound';
+import About from './pages/about/About';
+import Accomodation from './pages/accomodation/Accomodation';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: "/Kasa/",
+		element: <Home />
+	},
+	{
+		path: "/accomodation/:id",
+		element: <Accomodation />
+	},
+	{
+		path: '/about',
+		element: <About />
+	},
+	{
+		path: "*",
+		element: <NotFound />
+	},
+]);
 
 function App() {
-  
-  return (
-    <Router>
-      <div className='bodyHome'>
-
-        <Header/>
-        
-        <Routes>
-            <Route path="/details/:id"  element={<Details/>}></Route>
-            <Route path="/about" element={<About/>}></Route>
-            <Route path="/"  element={<Home/>}></Route>
-            <Route path="*" element={<NotFound/>}></Route>
-        </Routes>
-
-        <Footer/>
-        
-      </div>
-      
-    </Router>
-  )
+	return (
+		<>
+			<RouterProvider router={router}/>
+		</>
+	);
 }
 
 export default App;
